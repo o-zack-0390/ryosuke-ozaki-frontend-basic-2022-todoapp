f83200135bb3f721fe3a240f2243d4ae0b480e32
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-25 19:22:11 +0900

    Atoms/Checkbox を作成

--- src/components/Atoms/Checkbox/index.jsx
+++ src/components/Atoms/Checkbox/index.jsx
@@ -7,16 +7,14 @@ import "./style.css";
 export const Checkbox = ({onClick}) => {
   return (
     <Button onClick={onClick} className="checkbox">
-      <ButtonImage src={check} className="checkbox-image"/>
+      <ButtonImage src={check} alt="チェックボックスのチェックマーク" className="checkbox-image"/>
     </Button>
   );
 }
 
 const Button = styled.button`
-  background-color: ${COLOR.GREEN_TRANSPARENT};
-  &:hover {
-    background-color: ${COLOR.GREEN_TRANSLUCENT};
-  }
+  background-color: ${COLOR.LIGHT_GRAY_TRANSPARENT};
+  border: 2px solid ${COLOR.LIGHT_GRAY};
 `;
 
 const ButtonImage = styled.img``;
