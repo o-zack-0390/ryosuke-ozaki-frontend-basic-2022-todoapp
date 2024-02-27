33f47602116687b50a49bc0dea6412746fbfce26
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-02-27 17:38:16 +0900

    Organisms/TodoCardの雛形を修正

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import TEXTS from "../../../variables/texts";
-import COLOR from "../../../variables/color";
+import React from "react";
 import { Task } from "../../Molecules/Task/index"
 import "./style.css";
 
