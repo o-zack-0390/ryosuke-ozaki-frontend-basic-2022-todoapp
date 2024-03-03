e11f5be3272dcfb23b42f276c6b21427e0870baa
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-03-03 21:07:30 +0900

    classNameを削除

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -8,8 +8,8 @@ import "./style.css";
 
 export const AddTaskButton = ({onClick}) => {
   return (
-    <Button onClick={onClick} className="add-task-button">
-      <ButtonImage src={plus} className="add-task-button-image"/>
+    <Button onClick={onClick}>
+      <ButtonImage src={plus}/>
       <ButtonText className="add-task-button-text">タスクを追加</ButtonText>
     </Button>
   );
@@ -17,15 +17,33 @@ export const AddTaskButton = ({onClick}) => {
 
 const Button = styled.button`
   background-color: ${COLOR.GREEN_TRANSPARENT};
+  padding: 2px 6px;
+  border: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
   &:hover {
     background-color: ${COLOR.GREEN_TRANSLUCENT};
+    border-radius: 14px;
   }
 `;
+/*
+    button タグには初期値で border が設定されているので 0px に変更。
+    padding も初期値で設定されているので指定値に上書きする。
+*/
 
-const ButtonImage = styled.img``;
+const ButtonImage = styled.img`
+  height: 20px;
+`;
 
 const ButtonText = styled.p`
   ${TEXTS.S}
   color: ${COLOR.GREEN};
   font-family: ${FONTFAMILY.NOTO_SANS};
+  margin: 0px;
 `;
+/*
+  p タグには初期値で margin が設定されているので 0px に変更
+*/
