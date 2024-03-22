cfff621cea88241c21d597e8fcb1d41154c23d4e
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-03-22 22:39:04 +0900

    transition の位置を修正

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color"
-import BREAKPOINT from "../../../variables/breakpoint"
 import TEXTS from "../../../variables/texts"
 import FONTFAMILY from "../../../variables/font_family"
 
@@ -15,20 +14,16 @@ export const Alert = ({ isActive, context }) => {
 
 const AlertContainer = styled.div`
     position: absolute;
-    left: 50%;
     top: ${({alertActive}) => (alertActive ? `80px` : `0px`)};
+    left: 50%;
     width: 100%;
-    max-width: 360px;
+    max-width: 400px;
     padding: 10px 20px;
     border-radius: 4px;
     background: ${COLOR.RED};
     transition: all 0.5s ease 0s;
-    transform: translate(-50%, 0);
-    opacity: 1;
-
-    @media(max-width: ${BREAKPOINT.MEDIUM}){
-        transform: translate(-50%, 100%);
-    }
+    transform: ${({alertActive}) => (alertActive ? `translate(-50%, 0)` : `translate(-50%, -40%)`)};
+    visibility: ${({alertActive}) => (alertActive ? `visible` : `hidden`)};
 `;
 /*
     figma の通りに width = 400px にすると padding 含めて 440px になるので width = 360px に設定。
@@ -40,11 +35,12 @@ const AlertContainer = styled.div`
     ease       -> 最初と最後はゆっくり描画する。途中では速く描画する
     0s         -> トランジションの遅延を秒単位で指定
     transform  -> 要素を中央に配置
-    opacity    -> 不透明度
+    visibility -> レイアウトの表示設定 (visible:表示, hidden:非表示)
 */
 
 const AlertContext = styled.div`
     ${TEXTS.S};
     color: ${COLOR.WHITE};
     font-family: ${FONTFAMILY.NOTO_SANS};
+    transition: all 0.5s ease 0s;
 `;
