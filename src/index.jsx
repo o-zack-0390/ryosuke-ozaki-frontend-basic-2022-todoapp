18ec9cebec98c45b072f363c1d34550516575f27
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-03-22 18:38:52 +0900

    アラート機能を実装

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AlertHandlerProvider } from "./contexts/alert_handler";
 import { MainPage } from "./components/Pages/MainPage/index";
+import { AlertManager } from "./components/Organisms/AlertManager";
 import "./style.css";
 
 ReactDOM.render(
-    <MainPage />, 
+    <AlertHandlerProvider>
+        <MainPage />
+        <AlertManager />
+    </AlertHandlerProvider>
+    , 
     document.getElementById("app")
 );
