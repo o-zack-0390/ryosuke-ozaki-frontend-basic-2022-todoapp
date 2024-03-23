1bbdad3086ba1536e872c36091263b5cb8c468b6
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-03-23 20:17:59 +0900

    アラートが非表示になる位置を変更

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color"
-import TEXTS from "../../../variables/texts"
-import FONTFAMILY from "../../../variables/font_family"
+import TEXT from "../../../variables/texts"
 
 export const Alert = ({ isActive, context }) => {
     return (
@@ -14,31 +13,34 @@ export const Alert = ({ isActive, context }) => {
 
 const AlertContainer = styled.div`
     position: absolute;
-    top: ${({alertActive}) => (alertActive ? `80px` : `0px`)};
+    top: 80px;
     left: 50%;
     width: 100%;
     max-width: 400px;
     padding: 10px 20px;
+    background-color: ${COLOR.RED};
     border-radius: 4px;
-    background: ${COLOR.RED};
     transition: all 0.5s ease 0s;
-    transform: ${({alertActive}) => (alertActive ? `translate(-50%, 0)` : `translate(-50%, -40%)`)};
-    visibility: ${({alertActive}) => (alertActive ? `visible` : `hidden`)};
+    transform: translate(-50%, -40px);
+    visibility: hidden;
+    ${({ alertActive }) =>
+        alertActive && // alertActiveが true ならば後続の CSS スタイルを適用
+        `
+        transform: translate(-50%, 0px);
+        visibility: visible;
+    `}
 `;
 /*
-    top        -> Alert関数の isActive 変数を参照し、三項演算子でサイズを設定
-    transition -> トランジションを指定するプロパティ
-    all        -> 全プロパティに対してトランジションを適用
-    0.5s       -> トランジションタイムを秒単位で指定
-    ease       -> 最初と最後はゆっくり描画する。途中では速く描画する
-    0s         -> トランジションの遅延を秒単位で指定
-    transform  -> 要素を中央に配置
-    visibility -> レイアウトの表示設定 (visible:表示, hidden:非表示)
+    transition     -> トランジションを指定するプロパティ
+    all            -> 全プロパティに対してトランジションを適用
+    0.5s           -> トランジションタイムを秒単位で指定
+    ease           -> 最初と最後はゆっくり描画する。途中では速く描画する
+    0s             -> トランジションの遅延を秒単位で指定
+    transform      -> 要素を配置する座標
+    visibility     -> レイアウトの表示設定 (visible:表示, hidden:非表示)
 */
 
 const AlertContext = styled.div`
-    ${TEXTS.S};
+    ${TEXT.S};
     color: ${COLOR.WHITE};
-    font-family: ${FONTFAMILY.NOTO_SANS};
-    transition: all 0.5s ease 0s;
 `;
